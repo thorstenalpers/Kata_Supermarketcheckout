a90da85857dd468cc2b90e905256fdb8ad9d9827
thorstenalpers <ThorstenAlpers@web.de>
2019-01-29 17:59:55 +0100

    Add testcases for the checkout

--- SupermarketCheckout.Tests/Services/SupermarketCheckoutTests.cs
+++ SupermarketCheckout.Tests/Services/SupermarketCheckoutTests.cs
@@ -1,23 +1,35 @@
 using NSubstitute;
 using NUnit.Framework;
+using SupermarketCheckout.Model;
 using SupermarketCheckout.Services;
+using SupermarketCheckout.Services.Model;
+using System.Collections;
 
 namespace SupermarketCheckout.Services.Tests.Services
 {
     [TestFixture]
     public class SupermarketCheckoutTests
     {
-        [Test]
-        public void TestMethod1()
+        private readonly SupermarketBasketFactory _basketFactory = new SupermarketBasketFactory();
+
+        [TestCase(new EArticle[] { EArticle.Apple }, 30)]
+        [TestCase(new EArticle[] { EArticle.Banana }, 50)]
+        [TestCase(new EArticle[] { EArticle.Peach }, 60)]
+        [TestCase(new EArticle[] { EArticle.Apple, EArticle.Apple }, 45)]
+        [TestCase(new EArticle[] { EArticle.Banana, EArticle.Banana, EArticle.Banana }, 130)]
+        [TestCase(new EArticle[] { EArticle.Peach, EArticle.Peach }, 120)]
+        [TestCase(new EArticle[] { EArticle.Apple, EArticle.Apple, EArticle.Banana, EArticle.Banana, EArticle.Banana, EArticle.Peach, EArticle.Peach }, 295)]
+        public void CreateBill_TestCases_Success(EArticle[] articles, decimal expectedTotalPrice)
         {
             // Arrange
+            var basket = _basketFactory.Create(articles);
             var supermarketCheckout = this.CreateSupermarketCheckout();
-            var supermarketBasket = this.CreateSupermarketBasket();
 
             // Act
+            Bill bill = supermarketCheckout.CreateBill(basket);
 
             // Assert
-            Assert.Fail();
+            Assert.That(bill.TotalPrice, Is.EqualTo(expectedTotalPrice));
         }
 
         private SupermarketCheckout CreateSupermarketCheckout()
